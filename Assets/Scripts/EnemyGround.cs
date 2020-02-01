50d272cded78ad52a6f657c0eee4c05db45ee398
Shifat Khan <shifatkhan6@gmail.com>
2020-02-01 12:36:43 -0500

    added pathing

---
+++ Assets/Scripts/EnemyGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGround : Player
+{
+    public Transform target;
+
+    void Update()
+    {
+        // TODO: add 0.1 error checking to remove glitching
+        if(target.position.x < transform.position.x)
+        {
+            moveDirection = -1.0f;
+        }
+        else
+        {
+            moveDirection = 1.0f;
+        }
+
+        animator.SetFloat("runningSpeed", Mathf.Abs(moveDirection));
+    }
+}
