c8e92e28bf8e39d4790dc9a3880065fe3252f87e
DeeAndre Mac Andrew <34717537+ConcernedAnt@users.noreply.github.com>
2020-02-01 17:14:47 -0500

    temp health bar

---
+++ Assets/Scripts/BarSizeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarSizeManager : MonoBehaviour
+{
+    private Transform bar;
+
+    private void Start()
+    {
+        bar = transform.Find("bar");
+    }
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
