b0899540bf21190dc545d467d8976bfea2df978c
DeeAndre Mac Andrew <34717537+ConcernedAnt@users.noreply.github.com>
2020-02-02 15:27:53 -0500

    changes to karma bar

---
+++ Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Player player;
+    public Image fillImage;
+    private Slider slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+    void Update()
+    {
+        if (slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        if (slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+
+        // float fillValue = player.stamina / player.maxStamina;
+        float fillValue = 20;
+        slider.value = fillValue;
+    }
+}
